b49c2628f1b2b99da89fe1640609c8952f7435d0
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-09-20 21:31:30 -0400

    Server tests added

---
+++ test/server.js
@@ -0,0 +1,46 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('Color code converter API', function() {
+
+  describe('Convert RGB to HEX', function() {
+
+  let url = 'http://localhost:3000/rgbToHex?red=255&green=255&blue=255';
+  
+    it('Return status 200', function(done) {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);  
+        done();
+      });
+    }); 
+
+    it('Returns color in HEX', function(done) {
+      request(url, function(error, response, body) {
+        expect(body).to.equal('ffffff');
+        done();
+      });
+    }); 
+
+  });
+
+  describe('Convert HEX to RGB', function() {
+
+  let url = 'http://localhost:3000/hexToRgb?hex00ff00';
+  
+    it('Return status 200', function(done) {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);  
+        done();
+      });
+    }); 
+
+    it('Returns color in RGB', function(done) {
+      request(url, function(error, response, body) {
+        expect(body).to.equal('[0,255,0]');
+        done();
+      });
+    }); 
+    
+  });
+  
+});
